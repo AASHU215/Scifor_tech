7475389a842de8702f275e094ab1f598f47d8e19
AASHU215 <aashu0521@gmail.com>
2024-09-08 20:51:07 +0530

    Initial commit

---
+++ carrer/forms.py
@@ -0,0 +1,14 @@
+from django import forms
+from .models import Jobs, Application
+
+
+class JobForm(forms.ModelForm):
+    class Meta:
+        model = Jobs
+        fields = '__all__'
+
+
+class ApplicationForm(forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = '__all__'
